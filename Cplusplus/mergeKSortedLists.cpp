5cc8ef54e8fa535b0bd58d7837d4fad68a6b2811
Sahil Sharma <sahil.sharma@sjsu.edu>
2019-04-26 11:12:30 -0700

    merge K sorted lists divide and conquer

--- Cplusplus/mergeKSortedLists.cpp
+++ Cplusplus/mergeKSortedLists.cpp
@@ -1,3 +1,5 @@
+// USING MIN HEAP
+
 /**
  * Definition for singly-linked list.
  * struct ListNode {
@@ -51,6 +53,76 @@ public:
         }
         
         return result;
+        
+    }
+};
+
+
+
+// USING DIVIDE AND CONQUER
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+ListNode* merge( ListNode* l1, ListNode* l2 )
+{
+    ListNode* result = nullptr;
+    if ( l1 == nullptr )
+    {
+        return l2;
+    }
+    else if ( l2 == nullptr )
+    {
+        return l1;
+    }
+    
+    if ( l1->val <= l2->val )
+    {
+        
+        result = l1;
+        result->next = merge( l1->next, l2 );
+    }
+    else
+    {
+        result = l2;
+        result->next = merge( l1, l2->next );
+    }
+    
+    return result;
+}
+public:
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        if ( lists.size() == 0 )
+        {
+            return nullptr;
+        }
+        int last = lists.size() - 1;
+        
+        while ( last != 0 )
+        {
+            int i = 0;
+            int j = last;
+            
+            while ( i < j )
+            {
+                lists[i] = merge( lists[i], lists[j] );
+                i++, j--;
+                
+                if ( i >= j )
+                {
+                    last = j;
+                }
+            }
+            
+        }
+        
+        return lists[0];
         
     }
 };
