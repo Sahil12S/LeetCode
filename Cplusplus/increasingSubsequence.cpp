a06580cdb999add4da39594e4114f29af3e796e0
Sahil Sharma <sahil.sharma@sjsu.edu>
2019-05-15 17:18:51 -0700

    some modifications

--- Cplusplus/increasingSubsequence.cpp
+++ Cplusplus/increasingSubsequence.cpp
@@ -1,48 +1,23 @@
-// Given an unsorted array of integers, find the length of longest continuous increasing subsequence (subarray).
+/*
+Given an integer array, 
+your task is to find all the different possible increasing subsequences of the given array, 
+and the length of an increasing subsequence should be at least 2 .
+*/
 
 #include <iostream>
 #include <vector>
 
 using namespace std;
 
-int findLengthOfLCIS(vector<int>& nums);
+vector< vector<int>> findSubsequences( vector<int>& nums );
 
 int main(int argc, char const *argv[])
 {
-    // vector<int> nums = {1, 3, 5, 4, 7};
-    // vector<int> nums = {2, 2, 2, 2, 2};
-    vector<int> nums = {7,6,5,4,3,2};
-
-    cout << findLengthOfLCIS(nums) << endl;
-
+    
     return 0;
 }
 
-int findLengthOfLCIS(vector<int>& nums)
+vector< vector<int>> findSubsequences( vector<int>& nums )
 {
-    // if (nums.size() <= 1)
-    // {
-    //     return nums.size();
-    // }
-
-    int left = 0, iter = 0;
-    int maxLength = 0;
-
-    while (iter < nums.size())
-    {
-        if (iter + 1 < nums.size() && nums[iter + 1] > nums[iter])
-        {
-            iter++;
-        }
-        else
-        {
-            // cout << "nums: " << nums[iter] << endl;
-            maxLength = max(maxLength, (iter - left + 1));
-            left = ++iter;// + 1;
-            // iter++;
-        }
-        // cout << "left: " << left << ", iter: " << iter << endl;
-    }
-
-    return maxLength;
+    
 }
