1ad4c48da9493db9c39b84376b618525d177f649
Sahil Sharma <sahil.sharma@sjsu.edu>
2019-05-28 15:33:33 -0700

    add struct for treenode

--- Cplusplus/lowestCommonAncestor.cpp
+++ Cplusplus/lowestCommonAncestor.cpp
@@ -8,13 +8,14 @@
 
 /**
  * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
- * };
  */
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+};
 
 TreeNode *lowestCommonAncestor(TreeNode *root, TreeNode *p, TreeNode *q)
 {
