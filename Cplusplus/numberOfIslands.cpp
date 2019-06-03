0343c4bc41fc3f09be633079350d34f452321610
Sahil Sharma <sahil.sharma@sjsu.edu>
2019-06-03 12:16:18 -0700

    number of islands with bfs

--- Cplusplus/numberOfIslands.cpp
+++ Cplusplus/numberOfIslands.cpp
@@ -4,13 +4,22 @@
 
 #include <iostream>
 #include <vector>
+#include <queue>
 
-using namespace std;
+#define FORI(s, n) for (int i = s; i < n; i++)
+#define FORJ(s, n) for (int j = s; j < n; j++)
+#define FORK(s, n) for (int k = s; k < n; k++)
 
-int numOflands(vector< vector<char>>& grid);
-void dfsHelper(vector< vector<char>>& grid, int r, int c);
-void printGrid(vector< vector<char>> grid);
+using vi = std::vector<int>;
+using vvi = std::vector<vi>;
+using vc = std::vector<char>;
+using vvc = std::vector<vc>;
+using qc = std::queue<std::pair<int, int>>;
 
+bool isValidPoint(vvc &A, int i, int j);
+int numOflands2(vvc &grid);
+void dfsHelper(vvc &grid, int r, int c);
+void printGrid(vvc grid);
 
 int main(int argc, char const *argv[])
 {
@@ -20,24 +29,81 @@ int main(int argc, char const *argv[])
     //     {'1', '1', '0', '0', '0'},
     //     {'0', '0', '0', '0', '0'}
     // };
-    vector< vector<char>> island = {
+    vvc island = {
         {'1', '1', '0', '0', '0'},
         {'1', '1', '0', '0', '0'},
         {'0', '0', '1', '0', '0'},
-        {'0', '0', '0', '1', '1'}
-    };
+        {'0', '0', '0', '1', '1'}};
 
-    cout << "Number of islands: " << numOflands(island) << endl;
+    int count = numOflands2(island);
+    std::cout << "Number of islands: " << count << std::endl;
     return 0;
 }
 
-int numOflands(vector< vector<char>>& grid)
+int numOflands2(vvc &grid)
+{
+    int m = grid.size();
+    if (m == 0)
+    {
+        return -1;
+    }
+    int n = grid[0].size();
+
+    vvi dirs = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+
+    int count = 0;
+    FORI(0, m)
+    {
+        FORJ(0, n)
+        {
+            if (grid[i][j] == '1')
+            {
+                count++;
+                // std::cout << count << '\n';
+                qc q;
+                q.push({i, j});
+                grid[i][j] = '0';
+                while (!q.empty())
+                {
+                    int r = q.front().first;
+                    int c = q.front().second;
+                    q.pop();
+                    // std::cout << r << " " << c << " " << grid[r][c] << '\n';
+
+                    FORK(0, 4)
+                    {
+                        int nr = r + dirs[k][0];
+                        int nc = c + dirs[k][1];
+                        if (isValidPoint(grid, nr, nc) && grid[nr][nc] == '1')
+                        {
+                            // std::cout << "pushing: " << nr << " " << nc << '\n';
+                            q.push({nr, nc});
+                            grid[nr][nc] = '0';
+                        }
+                    }
+                }
+            }
+        }
+    }
+    return count;
+}
+
+bool isValidPoint(vvc &A, int i, int j)
+{
+    if (i < 0 || i >= A.size() || j < 0 || j >= A[0].size())
+    {
+        return false;
+    }
+    return true;
+}
+
+int numOflands(vvc &grid)
 {
     int numRows = grid.size();
-    
+
     if (!numRows)
         return 0;
-    
+
     int numColumns = grid[0].size();
 
     int numOfIslands = 0;
@@ -57,14 +123,12 @@ int numOflands(vector< vector<char>>& grid)
     return numOfIslands;
 }
 
-
-void dfsHelper(vector< vector<char>>& grid, int r, int c)
+void dfsHelper(vvc &grid, int r, int c)
 {
     int numRows = grid.size();
     int numColumns = grid[0].size();
 
     grid[r][c] = '0';
-
 
     if (r > 0 && grid[r - 1][c] == '1')
     {
@@ -84,17 +148,16 @@ void dfsHelper(vector< vector<char>>& grid, int r, int c)
     {
         dfsHelper(grid, r, c + 1);
     }
-
 }
 
-void printGrid(vector< vector<char>> grid)
+void printGrid(vvc grid)
 {
-    for (vector<char> vc : grid)
+    for (vc v : grid)
     {
-        for (char c : vc)
+        for (char c : v)
         {
-            cout << c << " ";
+            std::cout << c << " ";
         }
-        cout << endl;
+        std::cout << std::endl;
     }
 }
