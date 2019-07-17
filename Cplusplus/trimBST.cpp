942b16dd99269fc23f651a69f5ef2d7e5dbb16e9
Sahil Sharma <sahil.sharma@sjsu.edu>
2019-07-16 17:18:10 -0700

    correct format

--- Cplusplus/trimBST.cpp
+++ Cplusplus/trimBST.cpp
@@ -5,34 +5,35 @@
  * so the result should return the new root of the trimmed binary search tree.
  */
 
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
- * };
- */
-class Solution {
+// Definition for a binary tree node.
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+};
+
+class Solution
+{
 public:
-    TreeNode* trimBST(TreeNode* root, int L, int R) {
-        if ( root == nullptr )
+    TreeNode *trimBST(TreeNode *root, int L, int R)
+    {
+        if (root == nullptr)
         {
             return root;
         }
-        if ( root->val > R )
+        if (root->val > R)
         {
-            return trimBST( root->left, L, R );
+            return trimBST(root->left, L, R);
         }
-        if ( root->val < L )
+        if (root->val < L)
         {
-            return trimBST( root->right, L, R );
+            return trimBST(root->right, L, R);
         }
-        
-        
-        root->left = trimBST( root->left, L, R );
-        root->right = trimBST( root->right, L, R );
+
+        root->left = trimBST(root->left, L, R);
+        root->right = trimBST(root->right, L, R);
         return root;
     }
 };
