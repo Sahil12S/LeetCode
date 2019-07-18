872ac1b8bcfd81f7237ef31612746bae5d6330ee
Sahil Sharma <sahil.sharma@sjsu.edu>
2019-07-18 11:55:03 -0700

    closest bst value

--- Cplusplus/closestBSTValue.cpp
+++ Cplusplus/closestBSTValue.cpp
@@ -1,3 +1,11 @@
+/**
+ * Given a non-empty binary search tree and a target value, find the value in the BST that is closest to the target.
+ * 
+ * Note:
+ * 
+ * Given target value is a floating point.
+ * You are guaranteed to have only one unique value in the BST that is closest to the target.
+ */
 
 #include <algorithm>
 
@@ -7,7 +15,7 @@ struct TreeNode
     int val;
     TreeNode *left;
     TreeNode *right;
-    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
 };
 
 class Solution
