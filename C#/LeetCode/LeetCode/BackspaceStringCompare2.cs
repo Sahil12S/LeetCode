5e342204ad068e892b01ad5264cb113b2203c7cc
Sahil Sharma <sahilsharma260@gmail.com>
2020-04-22 21:20:45 -0700

    backspace string sequence 2
    VMWare question

---
+++ C#/LeetCode/LeetCode/BackspaceStringCompare2.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    class BackspaceStringCompare2
+    {
+        public Boolean ArraysKeyPress(string str1, string str2)
+        {
+            List<char> arrS = new List<char>();
+            List<char> arrT = new List<char>();
+
+            for (int i = 0; i < str1.Length; i++)
+            {
+                if (str1[i] == '\\' && arrS.Count > 0)
+                {
+                    arrS.RemoveAt(arrS.Count - 1);
+                    i++;
+                }
+                else if (str1[i] != '\\')
+                {
+                    arrS.Add(str1[i]);
+                }
+            }
+            for (int j = 0; j < str2.Length; j++)
+            {
+                if (str2[j] == '\\' && arrT.Count > 0)
+                {
+                    arrT.RemoveAt(arrT.Count - 1);
+                    j++;
+                }
+                else if (str2[j] != '\\')
+                {
+                    arrT.Add(str2[j]);
+                }
+            }
+            str1 = new string(arrS.ToArray());
+            str2 = new string(arrT.ToArray());
+
+            return str1.Equals(str2);
+        }
+    }
+}
--- C#/LeetCode/LeetCode/Solution.cs
+++ C#/LeetCode/LeetCode/Solution.cs
@@ -172,14 +172,14 @@ namespace LeetCode
             int[] nums = new int[] { 1, 1, 2 };
             Console.WriteLine(obj.ThirdMax(nums));*/
 
-            int[] arr = new int[] { 4, 5, 8, 2 };
+            /*int[] arr = new int[] { 4, 5, 8, 2 };
             int k = 3;
             KthLargest obj = new KthLargest(k, arr);
             Console.WriteLine(obj.Add(3));
             Console.WriteLine(obj.Add(5));
             Console.WriteLine(obj.Add(10));
             Console.WriteLine(obj.Add(9));
-            Console.WriteLine(obj.Add(4));
+            Console.WriteLine(obj.Add(4));*/
 
             /*MinHeap obj = new MinHeap(11);
             obj.Add(3);
@@ -194,6 +194,11 @@ namespace LeetCode
             Console.WriteLine(obj.Pop());
             obj.Add(-1);
             Console.WriteLine(obj.Peek());*/
+
+            string S = "XX\\b\\b\\b\\bXY";
+            string T = "XY";
+            BackspaceStringCompare2 obj = new BackspaceStringCompare2();
+            Console.WriteLine(obj.ArraysKeyPress(S, T));
         }
     }
 }
